7c98dd86b2cd2b8d4a43ff08dfdddb07e94c62e1
gungunsaluja <gungunsaluja16@gmail.com>
2024-03-30 21:10:15 +0530

    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,11 +4,24 @@ import logo from '../assets/Logo.svg'
 import {Link} from 'react-router-dom';
 const Navbar = () =>{
     return (
-        <div>
+        <div className = "flex">
             <Link to = "/">
-                <img src = {logo}>
+                <img src = {logo} alt = "Logo" width = {160} height = {32} loading = "lazy">
                 </img>
             </Link>
+            <nav>
+                <ul className='flex gap-3'>
+                    <li>
+                        <Link to ="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to ="/">About</Link>
+                    </li>
+                    <li>
+                        <Link to ="/">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
 
         </div>
         )
