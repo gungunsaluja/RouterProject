0297bee9df90f7c3e39e8eadeeac939e81e69f74
gungunsaluja <gungunsaluja16@gmail.com>
2024-03-31 01:04:38 +0530

    commit

--- src/Components/SignUpForm.jsx
+++ src/Components/SignUpForm.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 const SignUpForm = () => {
   const [formData, setFormData] = useState({
@@ -9,6 +10,7 @@ const SignUpForm = () => {
     confirmPassword: "",
   });
 
+  const [showPassword, setShowPassword] = useState(false);
   function changeHandler(event) {
     setFormData((prevData) => ({
       ...prevData,
@@ -23,22 +25,21 @@ const SignUpForm = () => {
         <button>Instructor</button>
       </div>
       <form>
-         {/* first name and lastname */}
+        {/* first name and lastname */}
         <div>
-           
           <label>
             <p>
               First Name<sup>*</sup>
             </p>
-          
-          <input
-            required
-            type="text"
-            name="firstname"
-            onChange={changeHandler}
-            placeholder="Enter First Name"
-            value={formData.firstName}
-          />
+
+            <input
+              required
+              type="text"
+              name="firstname"
+              onChange={changeHandler}
+              placeholder="Enter First Name"
+              value={formData.firstName}
+            />
           </label>
           <label>
             <p>
@@ -53,13 +54,56 @@ const SignUpForm = () => {
               value={formData.lastName}
             ></input>
           </label>
-          
         </div>
+        {/* email address */}
         <label>
-            <p>Email Address<sup>*</sup></p>
-            <input required type = "email" name = "email" 
-             onChange={changeHandler} placeholder="Enter Email Address" value = {formData.email}/>
+          <p>
+            Email Address<sup>*</sup>
+          </p>
+          <input
+            required
+            type="email"
+            name="email"
+            onChange={changeHandler}
+            placeholder="Enter Email Address"
+            value={formData.email}
+          />
+        </label>
+        {/* create password confirm password */}
+        <div>
+          <label>
+            <p>
+              Create Password<sup>*</sup>
+            </p>
+            <input
+              required
+              type={showPassword ? "text" : "password"}
+              name="password"
+              onChange={changeHandler}
+              placeholder="Enter Password"
+              value={formData.password}
+            />
+            <span onclick={() => setShowPassword((prev) => !prev)}></span>
           </label>
+
+          <label>
+            <p>
+              Confirm Password<sup>*</sup>
+            </p>
+            <input
+              required
+              type={showPassword ? "text" : "password"}
+              name="Confirmpassword"
+              onChange={changeHandler}
+              placeholder="Confirmpassword"
+              value={formData.confirmPassword}
+            />
+            <span onclick={() => setShowPassword((prev) => !prev)}></span>
+          </label>
+        </div>
+        <button>
+            Create button
+        </button>
       </form>
     </div>
   );
